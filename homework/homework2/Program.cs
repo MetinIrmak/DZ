1a405c48be587c2e7b70dea266690a9b0a4d15b8
MetinIrmak <124688826+MetinIrmak@users.noreply.github.com>
2023-02-19 21:35:02 +0300

    homework2

---
+++ homework/homework2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int Deletedecimal (int num)
+// {
+//     int sot = num % 100;
+//     return sot / 10;
+// }
+// Console.WriteLine("Input three-digit number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Deletedecimal(num));
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// Console.Write("Введите любое число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2)
+// {
+//   Console.WriteLine($"Третья цифра в числе {anyNumber} - " + anyNumberText[2]);
+// }
+// else
+//  {
+//   Console.WriteLine($"Третьей цифры в числе {anyNumber} - нет");
+// }
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("введите число от 1 до 7");
+// int num = Convert.ToInt32(Console.ReadLine());
+// {
+//     if (num > 0 && num < 6)
+//     {
+//     Console.WriteLine($"Нет. День {num} выходным не является");
+//     }
+//     else if (num > 5 && num < 8)
+//     {
+//     Console.WriteLine($"Да. День {num} является выходным");
+//     }
+//     else
+//     {
+//     Console.WriteLine($"Ошибка! Неправильно введенное число, введите число от 1 до 7!");
+//     }
+// }
---
+++ seminars/seminar2/Program.cs
@@ -0,0 +1,92 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// 78 -> 8
+
+// 12-> 2
+
+// 85 -> 8
+
+// void MaxDecimal (int num)
+// {
+//     int ed = num % 10;
+//     int dec = num / 10;
+//     if(ed>dec)
+//     {
+//         Console.WriteLine($"Большая цифра числа {num} -> {ed}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Большая цифра числа {num} -> {dec}");
+//     }
+// }
+
+// int num = new Random().Next(10, 99 + 1);
+
+// MaxDecimal(num);
+
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+
+// 782 -> 72
+
+// 918 -> 98
+
+// int Deletedecimal (int num)
+// {
+//     int sot = num / 100;
+//     int ed = num % 10;
+//     return sot*10 + ed;
+// }
+
+// int randomnumber = new Random().Next(100, 1000);
+// Console.WriteLine(randomnumber);
+// // int result = Deletedecimal(randomnumber);
+// // Console.WriteLine(result);
+// Console.WriteLine(Deletedecimal(randomnumber));
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+// void Devid723Decimal(int num)
+// {
+//     if (num % 7 == 0 && num % 23 == 0)
+//     {
+//     Console.WriteLine ("Да");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Нет");
+//     }
+// }
+
+// Console.WriteLine("Input num");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Devid723Decimal(num);
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если второе число не кратно числу первому, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+// void KratDecimal(double num1, double num2)
+// {
+//     if (num1 % num2 == 0)
+//     {
+//     Console.WriteLine("Кратно");    
+//     }
+//     else
+//     {
+//     Console.WriteLine($"Остаток от деления {num1} / {num2} = {num1 % num2}");
+//     } 
+// }
+// Console.WriteLine("Input firstnumber");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input secondnumber");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+
+// KratDecimal(num1, num2);

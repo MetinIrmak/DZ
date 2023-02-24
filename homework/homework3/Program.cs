1b1cabbb4d88cf027dd6337da655a885b8b3823b
MetinIrmak <124688826+MetinIrmak@users.noreply.github.com>
2023-02-24 19:17:02 +0300

    homework3

---
+++ homework/homework3/Program.cs
@@ -0,0 +1,72 @@
+﻿// ЗАДАЧА № 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// Console.WriteLine("Введите пятизначное число ");
+// string? text = Console.ReadLine();
+// char[] obrtext = text.ToCharArray();
+// Array.Reverse(obrtext);
+
+// string finaltext = new string(obrtext);
+
+// if(text==finaltext)
+//             {
+//                 Console.WriteLine($"Ура! число {text} является палиндромом!");
+//             }
+//             if (text!=finaltext)
+//             {
+//                 Console.WriteLine($"О нет! число {text} не является палиндромом!");
+//             }
+
+// ЗАДАЧА № 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// int x1 = Coordinate("x", "A");
+// int y1 = Coordinate("y", "A");
+// int z1 = Coordinate("z", "A");
+// int x2 = Coordinate("x", "B");
+// int y2 = Coordinate("y", "B");
+// int z2 = Coordinate("z", "B");
+
+// int Coordinate(string coorName, string pointName)
+// {
+//     Console.Write($"Введите координату {coorName} точки {pointName}: ");
+//     return Convert.ToInt16(Console.ReadLine());
+// }
+
+// double Decision(double x1, double x2, 
+//                 double y1, double y2, 
+//                 double z1, double z2){
+//   return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+//                    Math.Pow((y2-y1), 2) + 
+//                    Math.Pow((z2-z1), 2));
+// }
+
+// double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+// Console.WriteLine($"расстояние между двумя точками  {segmentLength}");
+
+// ЗАДАЧА № 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// int number = ReadInt("Введите число N: ");
+
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
+// }
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
---
+++ seminars/seminar3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задание 1 напишите программу которая принимает на вход координаты точек Х и У приче 
+
+// int FindQuart(double x, double y)
+// {
+// if(x>0 && y>0) 
+// {
+//     return 1;
+// }
+// if(x<0 && y>0) return 2;
+// if(x<0 && y<0) return 3;
+// if(x>0 && y<0) return 4;
+
+// return 0;
+// }
+// Console.WriteLine("Input X:");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input Y:");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(FindQuart(x,y));
+
+
+// double FindDistance (double xa, double ya, double xb, double yb);
+// {
+//     return Math.Round(Math.Sqrt(Math.Pow(xb-xa,2) + Math.Pow(yb-ya,2)),2);
+// }
+
+// Console.WriteLine("Input coordinate X for first point");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate Y for first point");
+// double ya = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate X for second point");
+// double xb = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input coordinate Y for second point");
+// double yb = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine($"Distanse between our points -> {FindDistance(xa,ya,xb,yb)}");

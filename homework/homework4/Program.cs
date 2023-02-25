d8583f8107837931ecab004ceacb8b0bdb5d2be5
MetinIrmak <124688826+MetinIrmak@users.noreply.github.com>
2023-02-25 15:39:01 +0300

    seminar + homework 4 completed

---
+++ homework/homework4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+// Console.WriteLine("Введите число А");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B т.е. степень в которую хотите возвести число А");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// Math.Pow(A, B);
+
+//  Console.WriteLine($"Число {A} в степени {B} = " + Math.Pow(A, B));
+// Console.ReadKey();
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// Console.WriteLine("Введите число");
+// int x = int.Parse(Console.ReadLine());
+// int sum=0;
+// for (int i=3; i>=0; i--)
+// {
+//     sum+=x/(int)Math.Pow(10.0,i);
+//     x=x%(int)Math.Pow(10.0,i);
+// }
+// Console.WriteLine(sum);
